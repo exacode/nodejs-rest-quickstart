09c5327b9e7afb9104d6e0e52110ff59b6df6770
mendlik <mendelski.pawel@gmail.com>
2014-02-04 23:53:09 +0100

    Modified logger configuration

--- app/modules/logger.js
+++ app/modules/logger.js
@@ -2,39 +2,42 @@
 
 var bunyan = require('bunyan');
 var config = require('./config/config.js');
-var logDir = __dirname + '/../../tmp/';
+var projectDir = __dirname + '/../../';
 
-switch (config.env) {
-	case 'production': {
-		var streams = [{
-			type: 'rotating-file',
-			path: logDir + config.env + '.log',
-			period: '1d',	// daily rotation
-			count: 7		// keep 3 back copies
-		}];
-	}
-	break;
-	default: {
-		var PrettyStream = require('bunyan-prettystream');
-		var prettyStdOut = new PrettyStream({
-			mode: 'short',
-			useColor: true
-		});
-		prettyStdOut.pipe(process.stdout);
-		var streams = [
-			{
-				level: 'trace',
+var streams = [];
+config.logger.streams.forEach(function(streamConf) {
+	switch (streamConf.type) {
+		case "file":
+			streams.push({
+				type: 'file',
+				path: (streamConf.path || './app.log').replace(/^\.\//, projectDir),
+				level: streamConf.level || 'trace'
+			});
+		break;
+		case "rotating-file":
+			streams.push({
+				type: 'rotating-file',
+				path: (streamConf.path || './app.log').replace(/^\.\//, projectDir),
+				period: streamConf.period || '1d',	// daily rotation
+				count: streamConf.count || 3,		// keep 3 back copies
+				level: streamConf.level || 'trace'
+			});
+		break;
+		default: // case "console":
+			var PrettyStream = require('bunyan-prettystream');
+			var prettyStdOut = new PrettyStream({
+				mode: streamConf.colors || 'short',
+				useColor: streamConf.colors || true
+			});
+			prettyStdOut.pipe(process.stdout);
+			streams.push({
 				type: 'raw',
-				stream: prettyStdOut
-			},
-			{
-				level: 'trace',
-				path: logDir + config.env + '.log',
-				type: 'file'
-			}
-		];
+				stream: prettyStdOut,
+				level: streamConf.level || 'trace'
+			});
+		break;
 	}
-}
+});
 
 exports.create = function(name) {
 	return bunyan.createLogger({
